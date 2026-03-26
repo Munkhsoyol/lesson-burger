2c7026ed2be4883b9c46c95d34fcf6e32da09a74
Munkhsoyol <munkhsoyoldavaa@gmail.com>
2026-03-26 09:37:25 +0800

    Updates

---
+++ precache-manifest.61a81b159c0b7bd957f61214fcbc14be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87532efe643be88552b57e7b7c1b1d4c",
+    "url": "/lesson-burger/index.html"
+  },
+  {
+    "revision": "e8f814193ce4e676caf4",
+    "url": "/lesson-burger/static/css/main.72922965.chunk.css"
+  },
+  {
+    "revision": "1603898b253c2580b6d2",
+    "url": "/lesson-burger/static/js/2.d4d8e97b.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/lesson-burger/static/js/2.d4d8e97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f814193ce4e676caf4",
+    "url": "/lesson-burger/static/js/main.30689722.chunk.js"
+  },
+  {
+    "revision": "0af5ff72ce66a96542a1",
+    "url": "/lesson-burger/static/js/runtime-main.33945b6a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/lesson-burger/static/media/burger-logo.b8503d26.png"
+  }
+]);

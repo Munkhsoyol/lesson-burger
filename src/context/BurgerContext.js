ce0ac697ef8e022774df8afdf34251ef2ecfebb5
Munkhsoyol <munkhsoyoldavaa@gmail.com>
2026-03-27 10:57:49 +0800

    107

--- src/context/BurgerContext.js
+++ src/context/BurgerContext.js
@@ -2,40 +2,65 @@ import React, { useState } from "react";
 
 const BurgerContext =  React.createContext();
 
+const INGREDIENT_PRICES = { salad: 150, cheese: 250, bacon: 800, meat: 1500 };
+
+const initialState = {
+  ingredients: {
+    salad: 0,
+    cheese: 0,
+    bacon: 0,
+    meat: 0
+  },
+  totalPrice: 1000,
+  purchasing: false,
+  ingredientNames: {
+    bacon: "Гахайн мах",
+    cheese: "Бяслаг",
+    meat: "Үхрийн мах",
+    salad: "Салад"
+  }
+};
+
 // Named export
 export const BurgerStore = (props) => {
-    const [ingredients, setIngredients] = useState({
-        salad: 0,
-        cheese: 0,
-        bacon: 0,
-        meat: 0
-    });
+    const [burger, setBurger] = useState( initialState );
 
     const addIngredient = (orts) => {
-        setIngredients({
-            ...ingredients,
-            [orts]: ingredients[orts] + 1
+        setBurger({
+            ...burger,
+            ingredients: {
+                ...burger.ingredients,
+                [orts]: burger.ingredients[orts] + 1
+            },
+            totalPrice: burger.totalPrice + INGREDIENT_PRICES[orts],
+            purchasing: true
         });
     };
 
     const removeIngredient = (orts) => {
-        setIngredients({
-            ...ingredients,
-            [orts]: ingredients[orts] - 1
+        const newPrice = burger.totalPrice - INGREDIENT_PRICES[orts];
+        setBurger({
+            ...burger,
+            ingredients: {
+                ...burger.ingredients,
+                [orts]: burger.ingredients[orts] - 1
+            },
+            totalPrice: newPrice,
+            purchasing: newPrice > 1000
         });
     };
 
     return (
         <BurgerContext.Provider 
             value={{
-                ingredients,
+                burger,
                 addIngredient,
                 removeIngredient
             }}
         >
             {props.children}
         </BurgerContext.Provider>
-    )
+    );
 };
 
 export default BurgerContext;
